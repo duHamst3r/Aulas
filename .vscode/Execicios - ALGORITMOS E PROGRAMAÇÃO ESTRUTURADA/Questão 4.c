722e0e83d91fdb3e05c82880aa0365b5bc4fbfde
duHamst3r <daviribas28@gmail.com>
2023-09-27 21:31:21 -0300

    commit

--- .vscode/Execicios - ALGORITMOS E PROGRAMAÇÃO ESTRUTURADA/Questão 4.c
+++ .vscode/Execicios - ALGORITMOS E PROGRAMAÇÃO ESTRUTURADA/Questão 4.c
@@ -6,13 +6,12 @@ int main() {
 
     // Leitura do nível de experiência
     printf("Digite o nível de experiência (a, b ou c): ");
-    scanf(" %c", &nivel); // Usamos um espaço antes de %c para ignorar espaços em branco e quebras de linha
+    scanf(" %c", &nivel);
 
     // Leitura do salário
-    // printf("Digite o salário atual: "); // Linha removida
     scanf("%f", &salario);
 
-    // Calcula o aumento com base no nível de experiência
+    // Calcular o aumento com base no nível de experiência
     switch (nivel) {
         case 'a':
             aumento = 0.05;
@@ -24,14 +23,17 @@ int main() {
             aumento = 0.08;
             break;
         default:
+        
+        //caso não inserir o nivel de experiência informar a seguinte mensagem
             printf("Nível de experiência inválido.\n");
-            return 1; // Encerra o programa com código de erro
+            return 1; 
+         
     }
 
-    // Calcula o salário atualizado
+    // Calcular o salário atualizado
     salario_atualizado = salario + (salario * aumento);
 
-    // Exibe o salário atualizado com 2 casas decimais
+    // Exibir o salário atualizado com 2 casas decimais
     printf("Salário atualizado: %.2f\n", salario_atualizado);
 
     return 0;
